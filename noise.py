df8b017b621552deb9451a34342b0be85d5c49a3
jacobcronce <jacobcronce@gmail.com>
2026-07-24 12:56:21 -0400

    Add files via upload

--- noise.py
+++ noise.py
@@ -11,28 +11,26 @@ class Noise:
     def superconducting_noise(self):
         #applies noise to a superconducting qubit architecture
         #simulate T1 energy relaxation and T2 dephasing
-        #return noisy circuit
+        #circuit object is modified for superconducting noise
         pass
-
-
 
     def trapped_ion_noise(self):
         #applies noise to a trapped ion qubit architecture
         #simulate laser fluctuations and crosstalk between ions
-        #return noisy circuit
+        #circuit object is modified for trapped ion noise
         pass
 
 
     def neutral_atom_noise(self):
         #applies noise to a neutral atom qubit architecture
         #simulate rydberg state decay and atom loss
-        #return noisy circuit
+        #circuit object is modified for neutral atom noise
         pass
 
     def photonic_noise(self):
         #applies noise to a photonic qubit architecture
         #simulate photon loss and mode mismatch
-        #return noisy circuit
+        #circuit object is modified for photonic noise
         pass
     
     
