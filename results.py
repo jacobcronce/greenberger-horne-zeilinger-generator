ca71098e280ae28c58473481289d7582472f2981
AryaVenkatesh2010 <prasana@alumni.princeton.edu>
2026-07-24 15:50:42 -0400

    new library for matrix operations + code for bunch of different methods

--- results.py
+++ results.py
@@ -1,5 +1,6 @@
 import cirq as cq
 import numpy as np
+import scipy.linalg #for the matrix operations
 
 class Results:
 
@@ -10,7 +11,8 @@ class Results:
 
     def __init__(self):
         self.statevector = None
-        self.density_matrix = cirq.DensityMatrixSimulator()
+        self.density_matrix = None
+        self.DM_Simulator = cirq.DensityMatrixSimulator()
         self.simulator = cirq.Simulator()
 
     def simulate_statevector(self, circuit):
@@ -21,39 +23,49 @@ class Results:
         #simulate a circuit and return the statevector
 
     def simulate_density_matrix(self, circuit):
-        toReturn = self.density_matrix.simulate(circuit)
+        toReturn = self.DM_Simulator.simulate(circuit)
         self.density_matrix = toReturn
         return self.density_matrix
-        pass
+        
         #simulate a circuit with noise and return the density matrix
 
     def density_matrix_from_statevector(self, statevector):
         result = self.density_matrix_from_state_vector(statevector)
         return result
-        pass
         #Creates a denstiy matrix from a statevector
+    
+    def fidelity(self, state1, state2):
 
-    def fidelity():
-        pass
+        sqrt_rho = scipy.linalg.sqrtm(state1)
+        fidelity = np.trace(scipy.linalg.sqrtm(sqrt_rho @ state2 @ sqrt_rho)) ** 2
+        return fidelity
+
+
         #calculate the fidelity between two quantum states
         #return a double
 
-    def von_neumann_entropy():
-        pass
+    def von_neumann_entropy(self, density_matrix):
+        eigenvalues = np.linalg.eigvalsh(density_matrix)
+        eigenvalues = eigenvalues[eigenvalues > 1e-12]  # Filter out small eigenvalues
+        entropy = -np.sum(eigenvalues * np.log(eigenvalues))
+        return entropy
+
         #calculate the von neumann entropy of a density matrix
         #return a double
 
-    def partial_trace():
-        pass
+    def partial_trace(self, density_matrix, keep, num_qubits):
+        return cirq.partial_trace(density_matrix.reshape([2] * num_qubits * 2), keep_indices=keep).reshape(2 ** len(keep), 2 ** len(keep))
+        #reshape that matrices that way they can fit into the cirq.partial_trace function and the final output
         #Compute partial trace of density matrix
-    
+        ##### Come back to this later.  
 
     def is_density_matrix():
         pass
         #determine if a matrix is a valid density matrix
 
-    def purity():
-        pass
+    def purity(self, density_matrix):
+        return np.trace(density_matrix @ density_matrix)
+        
         #Compute the purity
         #return a double
     def trace_distance():
