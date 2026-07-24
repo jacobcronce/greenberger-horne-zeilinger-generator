73b01b3a9bc9729e0ea72791244da97654a8332e
whyshlok-afk <whyshlok@gmail.com>
2026-07-24 13:01:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,18 @@ class Setup:
 
     def create_qubits(self):
         #create a list of qubits for the circuit
+        self.qubits=cirq.NamedQubit.range(self.num_qubits, prefix = "q")
         pass
 
     def create_circuit(self):
         #create a GHZ state circuit with the specified number of qubits
+        self.circuit=cirq.Circuit()
+        self.circuit.append(cirq.H(self.qubits[0]))
+        self.circuit.append(cirq.CNOT(self.qubits[0], self.qubits[1]))
+        self.circuit.append(cirq.CNOT(self.qubits[1], self.qubits[2]))                    
         pass
 
     def get_circuit(self):
         #return the created circuit
+        return self.circuit
         pass
